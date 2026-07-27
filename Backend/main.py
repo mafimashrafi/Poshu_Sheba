2ba05723cdce1ebc5532b9eb1c520b20106eac7b
mafimashrafi <mafimashrafi78@gmail.com>
2026-07-27 23:34:52 +0600

    Fixed import module, cehck issue

--- Backend/main.py
+++ Backend/main.py
@@ -7,11 +7,9 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(lifespan=lifespan)
 
-# Resolve absolute path to uploads directory
 UPLOAD_DIR = Path(__file__).resolve().parent / "uploads"
 UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
 app.mount("/uploads", StaticFiles(directory=str(UPLOAD_DIR)), name="uploads")
-
 
 
 @app.get("/", include_in_schema=False)
